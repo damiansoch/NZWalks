bd07819251764e9f3ed5d9e9c5260733e37d0a66
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-11-13 07:09:13 +0000

    using DTO typed manually

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
 using NZWalks.API.Repositories;
 
 namespace NZWalks.API.Controllers
@@ -7,15 +8,34 @@ namespace NZWalks.API.Controllers
     [Route("[controller]")]
     public class RegionsController : Controller
     {
+        private readonly IRegionsRepository regionsRepository;
 
-        public RegionsController(IRegionsRepository)
+        public RegionsController(IRegionsRepository regionsRepository)
         {
-
+            this.regionsRepository = regionsRepository;
         }
         [HttpGet]
        public IActionResult GetAllRegions()
         {
-
+            //getting domain regions
+            var allRegions= regionsRepository.GetAll();
+            //returning DTO regions
+            var regionsDTO = new List<Models.DTO.Region>();
+            allRegions.ToList().ForEach(region =>
+            {
+                var regionDTO = new Models.DTO.Region()
+                {
+                    Id = region.Id,
+                    Name = region.Name,
+                    Code = region.Code,
+                    Area = region.Area,
+                    Lat = region.Lat,
+                    Long = region.Long,
+                    Population = region.Population,
+                };
+                regionsDTO.Add(regionDTO);
+            });
+            return Ok(regionsDTO);
         }
     }
 }
